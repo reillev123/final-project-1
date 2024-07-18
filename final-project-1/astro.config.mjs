9adec7b96bde0709177daa7c77bfeeda79b4e528
reillev123 <evereilly1999@icloud.com>
2024-07-18 13:05:06 +0100

    adding deploy.yml

--- final-project-1/astro.config.mjs
+++ final-project-1/astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   
+  site: 'https://reillev123.github.io',
+  base: 'final-project-1',
+});
+
